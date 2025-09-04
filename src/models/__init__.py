cd2be353c57c1fd56d59886f81d4821a327f418e
smaskoun <63504489+smaskoun@users.noreply.github.com>
2025-09-03 21:17:05 -0400

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,12 @@
-# src/models/__init__.py
+# src/models/__init__.py - FINAL CORRECTED VERSION
 
 from flask_sqlalchemy import SQLAlchemy
 
 # Initialize the db object
 db = SQLAlchemy()
 
-# Import the User model so it's accessible to the rest of the app
-from .user import User 
+# Import all your models here to make them accessible to the rest of the app
+from .user import User
+from .brand_voice import BrandVoice
+# If you have other model files (like social_media.py), import their classes here too.
+# from .social_media import SocialMediaPost 
